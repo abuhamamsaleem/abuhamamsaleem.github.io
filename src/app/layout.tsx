98c03d489f85bb5e3b2ef041dd0eb1f52de1252a
abuhamamsaleem <saleem-abuhamam@outlook.com>
2025-09-27 12:56:49 +0400

    update description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Abuhamamsaleem",
-  description: "CV",
+  description: "Personal webpage",
 };
 
 export default function RootLayout({
